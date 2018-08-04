57f120b9fdee73714dc90c3ee6890e2473f7bb8a
Pureewat Kaewpoy <sgnsys3@Pureewats-MacBook-Pro.local>
2018-08-04 11:26:20 +0700

    Init source and test package

---
+++ src/java/xo/model/XO.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xo.model;
+
+/**
+ *
+ * @author sgnsys3
+ */
+public class XO {
+    private int player1Score;
+    private int player2Score;
+    private int tieScore;
+    private int turn;
+    private String[][] board;
+    private String currentPlayer;
+
+    public int getPlayer1Score() {
+        return player1Score;
+    }
+
+    public void setPlayer1Score(int player1Score) {
+        this.player1Score = player1Score;
+    }
+
+    public int getPlayer2Score() {
+        return player2Score;
+    }
+
+    public void setPlayer2Score(int player2Score) {
+        this.player2Score = player2Score;
+    }
+
+    public int getTieScore() {
+        return tieScore;
+    }
+
+    public void setTieScore(int tieScore) {
+        this.tieScore = tieScore;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+
+    public String[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(String[][] board) {
+        this.board = board;
+    }
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(String currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+}
