ab66a5a2edc08eb7da08ba3e301dc8875ccb9463
Pureewat Kaewpoy <sgnsys3@Pureewats-MacBook-Pro.local>
2018-08-04 12:58:45 +0700

    Finish increase player score.

--- src/java/xo/model/XO.java
+++ src/java/xo/model/XO.java
@@ -10,6 +10,7 @@ package xo.model;
  * @author sgnsys3
  */
 public class XO {
+
     private int player1Score;
     private int player2Score;
     private int tieScore;
@@ -63,5 +64,13 @@ public class XO {
 
     public void setCurrentPlayer(String currentPlayer) {
         this.currentPlayer = currentPlayer;
+    }
+
+    public void increasePlayerScore() {
+        if (currentPlayer == "X") {
+            player1Score++;
+        } else {
+            player2Score++;
+        }
     }
 }
